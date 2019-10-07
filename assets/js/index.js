6e513993e204f92224cef2578028971c59db8fbe
rapkin <mikola.parfenyuck@gmail.com>
2019-10-07 18:36:42 +0300

    Updates

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
-
-var swup = _interopDefault(require('swup'));
-var LazyLoad = _interopDefault(require('vanilla-lazyload'));
-
-new LazyLoad({ elements_selector: ".lazyload" });
-new swup();
